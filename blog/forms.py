177f0808faad1edf6ab616c4addadc4dc5df99c7
lagrandecode <seunogunmolufirst1@gmail.com>
2022-10-18 17:20:45 +0100

    "commit"

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import field, fields
+from django.forms import ModelForm
+from .models import Blog
+
+
+class BlogForm(ModelForm):
+    class Meta:
+        model = Blog
+        fields = '__all__'
